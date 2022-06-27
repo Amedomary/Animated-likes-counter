85a8c1e869d06c3055292bac7797bab634f6100e
Максим Гришин <maksim.grishin@irlix.ru>
2022-06-27 13:40:34 +0400

    test npm v02

--- dist/components/Button.js
+++ dist/components/Button.js
@@ -3,23 +3,198 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
+exports.LikeButtonAnimated = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+require("core-js/modules/web.dom-collections.iterator.js");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _Skeleton = require("@components/common/Skeletons/Skeleton");
 
-const Button = props => {
-  return /*#__PURE__*/_react.default.createElement("button", {
-    className: "btn btn--".concat(props.kind, " CTA"),
-    "data-id": props.id,
-    type: props.type,
-    name: props.name,
-    value: props.value,
-    disabled: props.disabled,
-    onClick: props.handleClick
-  }, /*#__PURE__*/_react.default.createElement("h4", null, props.label));
+var _SvgIcon = require("@components/common/SvgIcon");
+
+var _utils = require("@utils/utils");
+
+var _react = _interopRequireWildcard(require("react"));
+
+var _reactSpring = require("react-spring");
+
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+const NumberSpring = () => {
+  const rangeMass = [0.3, 2, 4.5, 6];
+  const [show, set] = (0, _react.useState)(true);
+  const {
+    current: elementMass
+  } = (0, _react.useRef)((0, _utils.randomNumber)(rangeMass[0], rangeMass[3]));
+  const weight = elementMass > rangeMass[2] ? 'bold' : elementMass < rangeMass[1] ? 'lighter' : 'normal';
+  const transitions = (0, _reactSpring.useTransition)(show, {
+    from: {
+      opacity: 0,
+      y: 0,
+      x: 0
+    },
+    enter: {
+      opacity: 1,
+      y: (0, _utils.randomNumber)(-20, -35),
+      x: (0, _utils.randomNumber)(-3, 3)
+    },
+    leave: {
+      opacity: 0
+    },
+    delay: 900,
+    config: {
+      mass: elementMass,
+      tension: 210,
+      friction: 20
+    },
+    onRest: () => set(false)
+  });
+  return transitions((styles, item) => item && /*#__PURE__*/_react.default.createElement(_reactSpring.animated.span, {
+    style: _objectSpread({
+      color: 'var(--text-level-2)',
+      fontWeight: weight,
+      position: 'absolute',
+      left: '27px'
+    }, styles)
+  }, "+1"));
 };
 
-var _default = Button;
-exports.default = _default;
+const HexSpringSVG = _ref => {
+  let {
+    id
+  } = _ref;
+  const aConfig = {
+    dur: '1s',
+    repeatCount: '1'
+  };
+  const {
+    current: height
+  } = (0, _react.useRef)((0, _utils.randomNumber)(40, 80));
+  const {
+    current: rotationCount
+  } = (0, _react.useRef)(Math.random() * 720);
+  return /*#__PURE__*/_react.default.createElement("svg", {
+    style: {
+      position: 'absolute',
+      pointerEvents: 'none',
+      top: 0,
+      zIndex: 100
+    },
+    width: "50",
+    height: "100",
+    viewBox: "0 0 50 100",
+    fill: "var(--c-accent)"
+  }, /*#__PURE__*/_react.default.createElement("g", {
+    id: "circle".concat(id),
+    transform: "translate(-7, -6)",
+    opacity: "0"
+  }, /*#__PURE__*/_react.default.createElement("g", null, /*#__PURE__*/_react.default.createElement("path", {
+    d: "M4.49737 2H11.5026L15 8L11.5026 14H4.49737L1 8L4.49737 2Z",
+    fill: "var(--bg-level-1)"
+  }), /*#__PURE__*/_react.default.createElement("path", {
+    fillRule: "evenodd",
+    clipRule: "evenodd" // eslint-disable-next-line max-len
+    ,
+    d: "M4.49737 2H11.5026L15 8L11.5026 14H4.49737L1 8L4.49737 2ZM5.18481 3.16129L2.36435 8L5.18481 12.8387H10.8152L13.6356 8L10.8152 3.16129H5.18481Z",
+    fill: "var(--c-accent)"
+  }), /*#__PURE__*/_react.default.createElement("path", {
+    fillRule: "evenodd",
+    clipRule: "evenodd",
+    d: "M9.7424 4.47657L11.3847 7.32116L10.379 7.90181L8.73669 5.05722L9.7424 4.47657Z",
+    fill: "var(--c-accent)"
+  }), /*#__PURE__*/_react.default.createElement("animateTransform", {
+    attributeName: "transform",
+    type: "rotate",
+    dur: "1s",
+    values: "0 8 8; ".concat(rotationCount, " 8 8;"),
+    repeatCount: "indefinite"
+  })), /*#__PURE__*/_react.default.createElement("animate", {
+    attributeName: "opacity",
+    values: "0;1;1;1;1;1;1;1;0",
+    dur: aConfig.dur,
+    repeatCount: aConfig.repeatCount
+  })), /*#__PURE__*/_react.default.createElement("animateMotion", {
+    fill: "freeze",
+    keyPoints: "0;1",
+    keyTimes: "0;1",
+    keySplines: "0.3 0.8 0.8 0.2",
+    dur: aConfig.dur,
+    calcMode: "spline",
+    begin: "0s",
+    repeatCount: aConfig.repeatCount,
+    origin: "10 10",
+    xlinkHref: "#circle".concat(id),
+    path: "M8 8C8 ".concat(height, " 27 ").concat(height, " 27 13")
+  }));
+};
+
+const LikeButtonAnimated = _ref2 => {
+  let {
+    disabledButton,
+    bem,
+    bemIcon,
+    handleLike,
+    isLoading,
+    likes,
+    bemCount
+  } = _ref2;
+  const [list, setList] = (0, _react.useState)([]);
+  const [countStyle, setCountStyle] = (0, _react.useState)({});
+  const durationCancel = 5000;
+  const durationPing = 100;
+  const durationIncomingAnimation = 1000;
+
+  const onLike = () => {
+    const now = new Date().getTime();
+    setList(a => [...a, now]);
+    setTimeout(() => {
+      setCountStyle({
+        transform: 'translateY(-1px)',
+        color: 'var(--c-accent)'
+      });
+      handleLike();
+      setTimeout(() => {
+        setCountStyle({});
+      }, durationPing);
+    }, durationIncomingAnimation);
+    setTimeout(() => {
+      setList(a => a.slice(1));
+    }, durationCancel);
+  };
+
+  const onHandleClick = () => {
+    onLike();
+  };
+
+  return /*#__PURE__*/_react.default.createElement("button", {
+    type: "button",
+    disabled: disabledButton,
+    className: bem,
+    onClick: onHandleClick,
+    style: {
+      position: 'relative'
+    }
+  }, list.length > 0 && list.map(e => /*#__PURE__*/_react.default.createElement(_react.default.Fragment, {
+    key: e
+  }, /*#__PURE__*/_react.default.createElement(HexSpringSVG, {
+    id: e
+  }), /*#__PURE__*/_react.default.createElement(NumberSpring, null))), /*#__PURE__*/_react.default.createElement(_SvgIcon.SvgIcon, {
+    type: "like",
+    className: bemIcon
+  }), /*#__PURE__*/_react.default.createElement(_Skeleton.TernaryForSkeleton, {
+    isLoading: isLoading,
+    width: "2rem"
+  }, /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("span", {
+    className: bemCount,
+    style: countStyle
+  }, likes))));
+};
+
+exports.LikeButtonAnimated = LikeButtonAnimated;
--- src/lib/components/Button.js
+++ src/lib/components/Button.js
@@ -1,16 +1,162 @@
-import React from 'react';
+import { TernaryForSkeleton } from '@components/common/Skeletons/Skeleton';
+import { SvgIcon } from '@components/common/SvgIcon';
+import { randomNumber } from '@utils/utils';
+import React, { useRef, useState } from 'react';
+import { animated, useTransition } from 'react-spring';
 
-const Button = (props) => {
+const NumberSpring = () => {
+  const rangeMass = [0.3, 2, 4.5, 6];
+  const [show, set] = useState(true);
+  const { current: elementMass } = useRef(randomNumber(rangeMass[0], rangeMass[3]));
+
+  const weight = elementMass > rangeMass[2] ? 'bold' : elementMass < rangeMass[1] ? 'lighter' : 'normal';
+
+  const transitions = useTransition(show, {
+    from: { opacity: 0, y: 0, x: 0 },
+    enter: { opacity: 1, y: randomNumber(-20, -35), x: randomNumber(-3, 3) },
+    leave: { opacity: 0 },
+    delay: 900,
+    config: { mass: elementMass, tension: 210, friction: 20 },
+    onRest: () => set(false),
+  });
+
+  return transitions(
+    (styles, item) =>
+      item && (
+        <animated.span
+          style={{ color: 'var(--text-level-2)', fontWeight: weight, position: 'absolute', left: '27px', ...styles }}
+        >
+          +1
+        </animated.span>
+      )
+  );
+};
+
+const HexSpringSVG = ({ id }) => {
+  const aConfig = {
+    dur: '1s',
+    repeatCount: '1',
+  };
+
+  const { current: height } = useRef(randomNumber(40, 80));
+  const { current: rotationCount } = useRef(Math.random() * 720);
+
   return (
-    <button className={`btn btn--${props.kind} CTA`}
-      data-id={props.id}
-      type={props.type}
-      name={props.name}
-      value={props.value}
-      disabled={props.disabled}
-      onClick={props.handleClick}>
-      <h4>{props.label}</h4>
+    <svg
+      style={{ position: 'absolute', pointerEvents: 'none', top: 0, zIndex: 100 }}
+      width="50"
+      height="100"
+      viewBox="0 0 50 100"
+      fill="var(--c-accent)"
+    >
+      <g id={`circle${id}`} transform="translate(-7, -6)" opacity="0">
+        <g>
+          <path d="M4.49737 2H11.5026L15 8L11.5026 14H4.49737L1 8L4.49737 2Z" fill="var(--bg-level-1)" />
+
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            // eslint-disable-next-line max-len
+            d="M4.49737 2H11.5026L15 8L11.5026 14H4.49737L1 8L4.49737 2ZM5.18481 3.16129L2.36435 8L5.18481 12.8387H10.8152L13.6356 8L10.8152 3.16129H5.18481Z"
+            fill="var(--c-accent)"
+          />
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M9.7424 4.47657L11.3847 7.32116L10.379 7.90181L8.73669 5.05722L9.7424 4.47657Z"
+            fill="var(--c-accent)"
+          />
+
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            dur="1s"
+            values={`0 8 8; ${rotationCount} 8 8;`}
+            repeatCount="indefinite"
+          />
+        </g>
+
+        <animate
+          attributeName="opacity"
+          values="0;1;1;1;1;1;1;1;0"
+          dur={aConfig.dur}
+          repeatCount={aConfig.repeatCount}
+        />
+      </g>
+
+      <animateMotion
+        fill="freeze"
+        keyPoints="0;1"
+        keyTimes="0;1"
+        keySplines="0.3 0.8 0.8 0.2"
+        dur={aConfig.dur}
+        calcMode="spline"
+        begin="0s"
+        repeatCount={aConfig.repeatCount}
+        origin="10 10"
+        xlinkHref={`#circle${id}`}
+        path={`M8 8C8 ${height} 27 ${height} 27 13`}
+      />
+    </svg>
+  );
+};
+
+export const LikeButtonAnimated = ({ disabledButton, bem, bemIcon, handleLike, isLoading, likes, bemCount }) => {
+  const [list, setList] = useState([]);
+  const [countStyle, setCountStyle] = useState({});
+  const durationCancel = 5000;
+  const durationPing = 100;
+  const durationIncomingAnimation = 1000;
+
+  const onLike = () => {
+    const now = new Date().getTime();
+    setList((a) => [...a, now]);
+
+    setTimeout(() => {
+      setCountStyle({
+        transform: 'translateY(-1px)',
+        color: 'var(--c-accent)',
+      });
+      handleLike();
+
+      setTimeout(() => {
+        setCountStyle({});
+      }, durationPing);
+    }, durationIncomingAnimation);
+
+    setTimeout(() => {
+      setList((a) => a.slice(1));
+    }, durationCancel);
+  };
+
+  const onHandleClick = () => {
+    onLike();
+  };
+
+  return (
+    <button
+      type="button"
+      disabled={disabledButton}
+      className={bem}
+      onClick={onHandleClick}
+      style={{ position: 'relative' }}
+    >
+      {list.length > 0 &&
+        list.map((e) => (
+          <React.Fragment key={e}>
+            <HexSpringSVG id={e} />
+            <NumberSpring />
+          </React.Fragment>
+        ))}
+
+      <SvgIcon type="like" className={bemIcon} />
+      <TernaryForSkeleton isLoading={isLoading} width="2rem">
+        <div>
+          <span className={bemCount} style={countStyle}>
+            {likes}
+          </span>
+        </div>
+      </TernaryForSkeleton>
     </button>
-  )
-}
-export default Button;
+  );
+};
